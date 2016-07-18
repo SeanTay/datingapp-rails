87e975d4cedc50f94c72114f12b974bfe15ff068
Michael Holzheimer <michael.r.holzheimer@gmail.com>
2016-07-18 10:41:07 -0400

    gems

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+profile = JSON.parse(File.read("db/profile.data.json"))
+Profile.destroy_all
+Profile.create!(profile)
